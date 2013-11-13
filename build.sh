b1a05cde6a90dc678835e51fbde36e93356c519d
Michał Hernas <michal@hernas.pl>
2013-11-13 15:27:36 +0100

    Removed came, changed build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-./build/bin/pelican -o ./output/en/ -s ./build/src/en_pelicanconf.py ./build/src/content/
-./build/bin/pelican -o ./output/de/ -s ./build/src/de_pelicanconf.py ./build/src/content/
+./build/bin/pelican -o ./ -s ./build/src/en_pelicanconf.py ./build/src/content/
+./build/bin/pelican -o ./de/ -s ./build/src/de_pelicanconf.py ./build/src/content/
