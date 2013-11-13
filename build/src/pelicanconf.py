ad5653e31e69267e07860f1e12225a874307ea63
Michał Hernas <michal@hernas.pl>
2013-11-13 14:33:02 +0100

    Aded new files, gitignore etc.

--- build/src/pelicanconf.py
+++ build/src/pelicanconf.py
@@ -3,13 +3,25 @@
 from __future__ import unicode_literals
 import os
 
-AUTHOR = u'andi'
-SITENAME = u'zerotired blog'
 SITEURL = ''
-
 TIMEZONE = 'Europe/Berlin'
 
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = u'de'
+
+if DEFAULT_LANG == 'de':
+	GRATIS_WORD = u'KOSTENLOSS'
+	AUTHOR = u'Inventorum Gmbh'
+	SITENAME = u'INVENTORUM - Wir l(i)eben Einzelhandel - Die iPad Kasse für den Einzelhandel.'
+	SITEKEYWORDS = u'kasse, registrierkasse, kassensystem, registrierkasse, kasse, ipadkasse, mobiles Kassen System, Einzelhandel, E-Commerce, ebay, Online Shop, Multi-Channel, GDPdu, 1-Touch, Publishing, Warenwirtschaft, Warenwirtschaftsystem, POS, iPhone, Kassensystem für iPhone'
+	SITEDESCRIPTION = u'Inventorum - Die Kasse und Warenwirtschaftssystem für iPhone, iPod touch und iPad.'
+
+if DEFAULT_LANG == 'en':
+	GRATIS_WORD = 'GRATIS'
+	AUTHOR = u'Inventorum Gmbh'
+	SITENAME = u'INVENTORUM - Retail is our Passion - The iPad Point of Sale for Retailers'
+	SITEKEYWORDS = u'point of sale, cash register, mobile, retail, E-Commerce, ebay, Online Shop, Multi-Channel, 1-Touch, Publishing, Inventory Management, POS, iPhone, iPad, iPad mini'
+	SITEDESCRIPTION = u'Inventorum - The point of sale / cash register for iPhone, iPod touch and iPad..'
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -26,12 +38,9 @@ LINKS =  (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 5
-
+DEFAULT_PAGINATION = 10
+STATIC_PATHS = ['documents', 'jobs']
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+#RELATIVE_URLS = True
 
-THEME = os.path.join(os.path.dirname(__file__), 'themes', 'zt')
-# CSS_FILE = 'm.min.css'
-CSS_FILE = 'main.less.css'
-# STATIC_PATHS = ['images', ]
+THEME = './themes/inventorum/'
