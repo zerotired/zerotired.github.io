86c1f5055cf4a999c647defb9e88c0d0cc4b5121
Bartosz Hernas <bartosz@hernas.pl>
2014-01-23 11:15:54 +0100

    Updated site contents

--- build/src/de_pelicanconf.py
+++ build/src/de_pelicanconf.py
@@ -8,3 +8,14 @@ AUTHOR = u'Inventorum Gmbh'
 SITENAME = u'INVENTORUM - Wir l(i)eben Einzelhandel - Die iPad Kasse für den Einzelhandel.'
 SITEKEYWORDS = u'kasse, registrierkasse, kassensystem, registrierkasse, kasse, ipadkasse, mobiles Kassen System, Einzelhandel, E-Commerce, ebay, Online Shop, Multi-Channel, GDPdu, 1-Touch, Publishing, Warenwirtschaft, Warenwirtschaftsystem, POS, iPhone, Kassensystem für iPhone'
 SITEDESCRIPTION = u'Inventorum - Die Kasse und Warenwirtschaftssystem für iPhone, iPod touch und iPad.'
+
+SITEURL_LANG = SITEURL + '/de'
+
+NAVBAR_TITLE_HOME = u'Start'
+NAVBAR_TITLE_FEATURES = u'Funktionen'
+NAVBAR_TITLE_PICTURES = u'Bilder'
+NAVBAR_TITLE_TESTIMONIALS = u'Kundenmeinungen'
+NAVBAR_TITLE_COMPANY = u'Unternehmen'
+NAVBAR_TITLE_CONTACT = u'Kontakt'
+
+NAVBAR_SOCIAL_EMAIL = u'E-Mail'
